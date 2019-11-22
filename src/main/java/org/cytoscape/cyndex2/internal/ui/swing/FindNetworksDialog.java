6de8ca496874b2267d25ec5d47763a1425557422
D <dotasek.dev@gmail.com>
2019-11-22 15:17:43 -0800

    Fix sorting regression

--- src/main/java/org/cytoscape/cyndex2/internal/ui/swing/FindNetworksDialog.java
+++ src/main/java/org/cytoscape/cyndex2/internal/ui/swing/FindNetworksDialog.java
@@ -221,8 +221,8 @@ public class FindNetworksDialog extends javax.swing.JDialog implements PropertyC
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Find Networks");
-        setPreferredSize(new java.awt.Dimension(909, 550));
 
+        resultsTable.setAutoCreateRowSorter(true);
         resultsTable.setIntercellSpacing(new java.awt.Dimension(6, 2));
         resultsTable.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         jScrollPane1.setViewportView(resultsTable);
