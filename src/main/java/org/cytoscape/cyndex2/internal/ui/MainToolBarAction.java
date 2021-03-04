7000065264d5e1ec1fdf85c66657255bea2e839b
dotasek <dotasek.dev@gmail.com>
2021-03-04 11:59:58 -0500

    Add first menu item access to My Networks or Sign-In

--- src/main/java/org/cytoscape/cyndex2/internal/ui/MainToolBarAction.java
+++ src/main/java/org/cytoscape/cyndex2/internal/ui/MainToolBarAction.java
@@ -5,12 +5,18 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.Icon;
 import javax.swing.JComponent;
+import javax.swing.JFrame;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
 import org.cytoscape.application.swing.AbstractCyAction;
 import org.cytoscape.application.swing.CySwingApplication;
+import org.cytoscape.cyndex2.internal.rest.parameter.LoadParameters;
+import org.cytoscape.cyndex2.internal.ui.swing.FindNetworksDialog;
+import org.cytoscape.cyndex2.internal.ui.swing.SignInDialog;
 import org.cytoscape.cyndex2.internal.util.IconUtil;
+import org.cytoscape.cyndex2.internal.util.Server;
+import org.cytoscape.cyndex2.internal.util.ServerManager;
 import org.cytoscape.cyndex2.internal.util.StringResources;
 import org.cytoscape.service.util.CyServiceRegistrar;
 import org.cytoscape.util.swing.LookAndFeelUtil;
@@ -64,6 +70,41 @@ public class MainToolBarAction extends AbstractCyAction {
 		DialogTaskManager taskManager = serviceRegistrar.getService(DialogTaskManager.class);
 		Font font = IconUtil.getAppFont(23f);
 		int iconSize = 24;
+		
+		final Server selectedServer = ServerManager.INSTANCE.getSelectedServer();
+		final JMenuItem topMenuItem;
+		
+	
+		final Icon accountIcon = new TextIcon(IconUtil.ICON_NDEX_ACCOUNT, font, IconUtil.ICON_COLOR_2, iconSize, iconSize);
+		
+    	if (selectedServer == null) {
+			topMenuItem = new JMenuItem("Get on NDEx...", accountIcon);
+			topMenuItem.addActionListener(evy -> {
+				JFrame jframe = serviceRegistrar.getService(CySwingApplication.class).getJFrame();
+				SignInDialog signInDialog = new SignInDialog(null);
+				signInDialog.setLocationRelativeTo(jframe);
+				signInDialog.setVisible(true);
+				
+				final Server newSelectedServer = ServerManager.INSTANCE.getSelectedServer();
+				if (newSelectedServer != null) {
+					final LoadParameters loadParameters = new LoadParameters();
+					loadParameters.searchTerm = "";
+					loadParameters.userNetworksOnly = false;
+					FindNetworksDialog.getFindNetworksDialog(loadParameters);
+				}	
+			});
+		} else {
+			topMenuItem = new JMenuItem("My Networks...", accountIcon);
+			topMenuItem.addActionListener(evt -> {
+				final LoadParameters loadParameters = new LoadParameters();
+				loadParameters.searchTerm = "";
+				loadParameters.userNetworksOnly = true;
+				FindNetworksDialog.getFindNetworksDialog(loadParameters);
+			});	
+		}
+		popup.add(topMenuItem);
+		
+		popup.addSeparator();
 		
 		{
 			Icon icon = new TextIcon(IconUtil.LAYERED_OPEN_ICON, font, IconUtil.LAYERED_OPEN_SAVE_COLORS, iconSize, iconSize, 1);
